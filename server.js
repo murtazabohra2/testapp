a459e59e99158bb8c30bde538dd6810317cc8a28
murtazabohra2 <murtazabohra2@gmail.com>
2014-12-12 11:36:21 +0530

    commit

---
+++ server.js
@@ -0,0 +1,24 @@
+var port =  process.env.OPENSHIFT_NODEJS_PORT || 8080;   // Port 8080 if you run locally
+var address =  process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"; // Listening to localhost if you run locally
+
+var io = require('socket.io').listen(port, address, {
+	'browser client minification': true
+});
+
+io.sockets.on('connection', function (socket) {
+
+	var user;
+
+	socket.on('register', function (u) {
+		user = u;
+		socket.emit('message', {
+			nickname: '** SERVER **',
+			message: 'Welcome to chat ' + user.nickname + '!'
+		});
+	});
+
+	socket.on('message', function (m) {
+		socket.broadcast.emit('message', m);
+	});
+
+});
