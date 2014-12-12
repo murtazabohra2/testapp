c4f9ede9ad22b9bb69abc8d980cbf63926ea08d2
murtazabohra2 <murtazabohra2@gmail.com>
2014-12-12 11:40:57 +0530

    commit

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 var port =  process.env.OPENSHIFT_NODEJS_PORT || 8080;   // Port 8080 if you run locally
 var address =  process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1"; // Listening to localhost if you run locally
 
-var io = require('socket.io').listen(port, address, {
-	'browser client minification': true
-});
+var io = require('socket.io').listen(port, address);
 
 io.sockets.on('connection', function (socket) {
 
